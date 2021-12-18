713527237885ed7b65e77373d2725771ac80b687
Jairo Ortiz <58676509+j0rtiz@users.noreply.github.com>
2021-12-17 21:02:05 -0300

    fix(script): config

--- vm.ps1
+++ vm.ps1
@@ -4,11 +4,17 @@ if ($vm -eq $null) {
   $vm = "dev"
 }
 
+echo "# Config..."
+$config = $args[1]
+if ($config -eq $null) {
+  $config = "vm.yaml"
+}
+
 echo "# Delete..."
 multipass delete -p $vm
 
 echo "# Launch..."
-multipass launch -n $vm -c 2 -m 2G -d 20G --cloud-init vm.yaml
+multipass launch -n $vm -c 2 -m 2G -d 20G --cloud-init $config
 
 echo "# IP..."
 $ip = (multipass info $vm --format csv | ConvertFrom-Csv).Ipv4
