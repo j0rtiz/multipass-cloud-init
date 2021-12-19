542f14531869f48c9acb49600ba3ff9cad5f9ebf
Jairo Ortiz <58676509+j0rtiz@users.noreply.github.com>
2021-12-19 00:09:38 -0300

    refactor(script): browser step removal

--- vm.ps1
+++ vm.ps1
@@ -21,12 +21,6 @@ multipass delete -p $vm
 echo "# Launch..."
 multipass launch -n $vm -c 2 -m 2G -d 20G --cloud-init $config
 
-echo "# IP..."
-$ip = (multipass info $vm --format csv | ConvertFrom-Csv).Ipv4
-
-echo "# Browser..."
-start https://${ip}:3000/api/docs/
-
 echo "# Shell..."
 multipass sh $vm
 
